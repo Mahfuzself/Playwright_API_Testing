064f874a6a5ea20ee7694f2df924bdcdd400281f
Md Mahfuz Alam <mdmahfuzalam196@gmail.com>
2025-09-14 13:57:13 +0600

    Dynamic data from json

--- tests/api_test/post_request.ts
+++ tests/api_test/post_request.ts
@@ -1,34 +1,28 @@
 import {test,expect} from '@playwright/test';
 import 'dotenv/config';
 import postAPIRequest from '../../utils/test_data/api_request/post_request.json';
-// test("Create Post API request using static request body", async ({request}) => {
-//     const response = await request.post('/booking', {
-//         data: {
-//             "firstname": "Sally",
-//             "lastname": "Brown",
-//             "totalprice": 111,
-//             "depositpaid": true,
-//             "bookingdates": {
-//                 "checkin": "2013-02-23",
-//                 "checkout": "2014-10-23"
-//             },
-//             "additionalneeds": "Breakfast"
-//         }
-//     });
-//     console.log("Full body : "+(response));
+test.use({
+    baseURL: process.env.BASE_URL
+})
+test("Create Post API request using static request body", async ({request}) => {
+    const response = await request.post('/booking', {
+        data : postAPIRequest.booking
+    });
+    const responseBody = await response.json();
+    console.log(`Response Body: ${JSON.stringify(responseBody,null,2 )}`);
+    // console.log("Full body : "+(response));
 
-//     expect(response.ok()).toBeTruthy();
-//     expect(response.status()).toBe(200);
-//     const responseBody = await response.json();
-//     expect(responseBody.booking.firstname).toBe("Sally");
-//     expect(responseBody.booking.lastname).toBe("Brown");
-// })
+    // expect(response.ok()).toBeTruthy();
+    // expect(response.status()).toBe(200);
+    // const responseBody = await response.json();
+    // expect(responseBody.booking.firstname).toBe("Sally");
+    // expect(responseBody.booking.lastname).toBe("Brown");
+})
 
 test("Login API with valid credentials should return 200", async ({ request }) => {
   // Use the exact payload that triggers 401 in manual hit
   const payload = {
-    email: "andrewlynn@yopmail.com", // likely field expected is 'email', not 'username'
-    password: "Test@123"             //  password
+    data: postAPIRequest.login.valid,     //  password
   };
 
   // Make POST request
@@ -39,17 +33,17 @@ test("Login API with valid credentials should return 200", async ({ request }) =
         "Accept": "application/json",
         "Content-Type": "application/json",
       },
-      data: payload,
+      data: postAPIRequest.login.valid,
     }
   );
 
   // Log for debugging
   console.log("Status:", response.status());
   const responseBody = await response.json();
-  console.log("Body:", responseBody);
+  console.log(`Body: ${JSON.stringify(responseBody,null,2 )}`);
 
-  // Assert status code is 401
-  expect(response.status()).toBe(401);
+  // Assert status code is 200
+  expect(response.status()).toBe(200);
 
   // Optional: assert the error message
   // expect(responseBody).toHaveProperty('message');
@@ -77,7 +71,7 @@ test("Login API with incorrect credentials should return 401", async ({ request 
   // Log for debugging
   console.log("Status:", response.status());
   const responseBody = await response.json();
-  // console.log("Body:", responseBody);
+  console.log("Body:", responseBody);
 
   // Assert status code is 401
   expect(response.status()).toBe(401);
@@ -85,4 +79,35 @@ test("Login API with incorrect credentials should return 401", async ({ request 
   // Optional: assert the error message
   expect(responseBody).toHaveProperty('message');
   expect(responseBody.message.toLowerCase()).toContain('the provided credentials are not correct');
+});
+test("Login API with no-otp should return 200", async ({ request }) => {
+  // Use the exact payload that triggers 401 in manual hit
+  const payload = {
+    email: "rl_mahfuz108@yopmail.com", // likely field expected is 'email', not 'username'
+    password: "Test@12345"             //  password
+  };
+
+  // Make POST request
+  const response = await request.post(
+    "auth/sign-in",
+    {
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      data: payload,
+    }
+  );
+
+  // Log for debugging
+  console.log("Status:", response.status());
+  const responseBody = await response.json();
+  console.log("Body:", responseBody);
+
+  // Assert status code is 401
+  expect(response.status()).toBe(200);
+
+  // // Optional: assert the error message
+  // expect(responseBody).toHaveProperty('message');
+  // expect(responseBody.message.toLowerCase()).toContain('the provided credentials are not correct');
 });
